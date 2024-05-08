afbf0e36555e18baf9df116900a284604e04a15c
aishwaryax <aishsahoo5@gmail.com>
2024-05-08 00:19:09 -0400

    Update download.py

--- download.py
+++ download.py
@@ -1,10 +1,10 @@
 import gdown
 
 # Define the URL of the zip file on Google Drive
-url = 'https://drive.google.com/file/d/10F1SMXpriqA1FVLieUjihb94Jaj8nwHt/view?usp=drive_link'
+url = 'https://drive.google.com/uc?id=10F1SMXpriqA1FVLieUjihb94Jaj8nwHt'
 
 # Define the output file name for the downloaded zip file
-output = 'sft_sft_0_202404201349_step20000.pt'
+output = 'sft.zip'
 
 # Download the zip file using gdown
 gdown.download(url, output, quiet=False)
