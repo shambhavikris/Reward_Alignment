8d64d853cbcaa8da1ccd762b1a5574bee6c040a0
aishwaryax <aishsahoo5@gmail.com>
2024-05-07 22:00:40 -0400

    Change script to incorporate SFT model as base model

--- train_reward_model.sh
+++ train_reward_model.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 
-python3 train_rm.py -b 16 -n FRFT_iter1 -g ./sft_sft_0_202404201349_step20000.pt
+python3 train_rm.py -b 16 -n FRFT_iter1 -p ./sft_sft_0_202404201349_step20000.pt -g ./sft_sft_0_202404201349_step20000.pt
